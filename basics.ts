aafc5f86b58c039b9857065900bb9efd58e5bad7
kevit-vasanti-vaghela <vasanti.vaghela@kevit.io>
2023-03-16 19:01:33 +0530

    types declaration and definition

---
+++ basics.ts
@@ -0,0 +1,41 @@
+// Primitives: number, string, boolean
+// More complex types: arrays, objects
+// Function types, parameters
+
+// Primitives
+
+let age: number;
+
+let score: number = 20;  // gtivalid
+
+
+age = 12;
+
+let userName: string;
+
+userName = 'Max';
+
+let isInstructor: boolean;
+
+isInstructor = true;
+
+// Complex types
+
+let hobbies: string[];
+
+hobbies = ['Sports', 'Cooking'];
+
+let person: {
+    name: string,
+    age: number; 
+};                  // single object
+
+person = {
+    name: 'Max',
+    age: 32
+}
+
+let people: {
+    name: string,
+    age: number;
+}[];                 // array of objects
