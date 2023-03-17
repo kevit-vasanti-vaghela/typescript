1b7181d7651fcbbb69a812dd193be942dcfb4ff6
kevit-vasanti-vaghela <vasanti.vaghela@kevit.io>
2023-03-17 19:33:38 +0530

    inpute field reset

--- react-ts/src/components/NewTodo.tsx
+++ react-ts/src/components/NewTodo.tsx
@@ -15,6 +15,7 @@ const NewTodo: React.FC<{onAddTodo: (text: string) => void}> = (props) => {
         }
 
         props.onAddTodo(enteredText);
+        todoTextInputRef.current!.value = ''
     };
 
   return (
