37a8ce7a50b06d4b2c5626d6c5378343a84d55fa
kevit-vasanti-vaghela <vasanti.vaghela@kevit.io>
2023-03-17 16:27:31 +0530

    defined function type

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,14 +1,21 @@
 import Todos from './components/Todos';
 import './App.css';
 import Todo from './models/todo';
+import NewTodo from './components/NewTodo';
 
 function App() {
   const todos = [
     new Todo('Learn React'),
     new Todo('Learn TypeScript'),
   ];
+
+  const addTodoHandler = (todoText: string) => {
+
+  }
+
   return (
     <div>
+      <NewTodo onAddTodo={addTodoHandler} />
       <Todos items={todos} />
     </div>
   );
