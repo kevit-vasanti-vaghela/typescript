ac83b68f9e743b037ae04ea9870acec929913958
kevit-vasanti-vaghela <vasanti.vaghela@kevit.io>
2023-03-21 11:15:04 +0530

    context of todos created also children type manually definied

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,31 +1,16 @@
-import { useState } from 'react';
 import Todos from './components/Todos';
 import './App.css';
-import Todo from './models/todo';
 import NewTodo from './components/NewTodo';
+import TodosContextProvider from './store/todos-context';
 
 function App() {
-  const [todos, setTodos] = useState<Todo[]>([]);
-
-  const addTodoHandler = (todoText: string) => {
-    const newTodo = new Todo(todoText);
-
-    setTodos((prevTodos) => {
-      return prevTodos.concat(newTodo);
-    })
-  } 
-
-  const deleteTodoHandler = (id: string) => {
-    setTodos((prevTodos) => {
-      return prevTodos.filter(todo => todo.id !== id);
-    })
-  }
+ 
 
   return (
-    <div>
-      <NewTodo onAddTodo={addTodoHandler} />
-      <Todos items={todos} onDeleteTodo={deleteTodoHandler}/>
-    </div>
+    <TodosContextProvider>
+      <NewTodo />
+      <Todos />
+    </TodosContextProvider>
   );
 }
 
--- react-ts/src/components/Todos.tsx
+++ react-ts/src/components/Todos.tsx
@@ -1,14 +1,17 @@
-import Todo from "../models/todo";
+import React, { useContext } from "react";
+import { TodosContext } from "../store/todos-context";
 import TodoItem from "./TodoItem";
 import classes from './Todo.module.css'
 
 
-const Todos: React.FC<{ items: Todo[], onDeleteTodo: (id: string) => void }> = (props) =>  {
+const Todos: React.FC = () =>  {
+    const todosCtx = useContext(TodosContext);
+
     return (
         <ul className={classes.todos}>
             {
-                props.items.map((item) => (
-                    <TodoItem key={item.id} text={item.text} onDeleteTodo={props.onDeleteTodo.bind(null, item.id)}/>
+                todosCtx.items.map((item) => (
+                    <TodoItem key={item.id} text={item.text} onDeleteTodo={todosCtx.removeTodo.bind(null, item.id)}/>
                 ))
             }
         </ul>
